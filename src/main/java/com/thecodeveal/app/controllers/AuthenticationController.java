91f339acc6f278b2d2bffcd1e856a660095b88e9
Kanyesigye2002 <kanyeallan@gmail.com>
2021-07-03 17:49:58 +0300

    commit3

--- src/main/java/com/thecodeveal/app/controllers/AuthenticationController.java
+++ src/main/java/com/thecodeveal/app/controllers/AuthenticationController.java
@@ -61,8 +61,6 @@ public class AuthenticationController {
 		User userObj=(User) userDetailsService.loadUserByUsername(user.getName());
 		
 		UserInfo userInfo=new UserInfo();
-		userInfo.setFirstName(userObj.getFirstName());
-		userInfo.setLastName(userObj.getLastName());
 		userInfo.setRoles(userObj.getAuthorities().toArray());
 		
 		
