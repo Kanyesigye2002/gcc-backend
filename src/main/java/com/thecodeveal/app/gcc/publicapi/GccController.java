d46f3f147ac18187481f888e32579a29e56c11cd
allankanye <kanyeallan@gmail.com>
2021-07-22 17:56:45 +0300

    commit pro

--- src/main/java/com/thecodeveal/app/gcc/publicapi/GccController.java
+++ src/main/java/com/thecodeveal/app/gcc/publicapi/GccController.java
@@ -16,7 +16,7 @@ import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(value = {"http://localhost:3000","http://localhost:3001", "https://gcc-app-ug.herokuapp.com/", "https://main.d2ia5wwug43lbw.amplifyapp.com"})
+@CrossOrigin(value = {"http://localhost:3000","https://master.d1zaj6z68k0ehd.amplifyapp.com", "https://gcc-app-ug.herokuapp.com/", "https://main.d2ia5wwug43lbw.amplifyapp.com"})
 //@CrossOrigin(value = "https://gloriousch.herokuapp.com/")
 @RestController
 @RequestMapping("api/gcc/v1")
